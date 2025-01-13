0cd357d29f603cfa65b5528d92c22ea37507f973
Ron Boutilier <ronbout@yahoo.com>
2025-01-12 22:28:27 -0600

    Remove link to ronboutilier.com in footer as that is no longer mine.  Ironically, it looks like a guy who codes fitness software and lifts!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
 	return (
 		<footer>
 			<p id="footer-sig">
-				written and coded by&nbsp;
-				<a href="http://ronboutilier.com" target="_blank">
+				designed and coded by&nbsp;
+				<a href="#" target="_blank">
 					Ron Boutilier
 				</a>
 			</p>
